c0ef7cfe6dc35a5cefdbc2a7f0534752b59cf39a
Marcel Zielinski <marziel2001@gmail.com>
2023-12-30 21:39:53 +0100

    Added router

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import MainButtonContainer from "../Components/components/MainButtonContainer";
+import MainSideContent from "../Components/components/MainSideContent";
+
+const Home = () => {
+    return (
+        <>
+            <h1> This is home </h1>
+            <MainSideContent />
+        </>
+    )
+};
+
+export default Home;
---
+++ src/pages/levels.js
@@ -0,0 +1,9 @@
+const Level = () => {
+    return (
+        <>
+            <h1> This is level view </h1>
+        </>
+    )
+};
+
+export default Level;
