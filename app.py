064228b85f7570507cebcd902fe9d9bee3e17710
Łukasz Prahl <102081415+NOTSAHLO@users.noreply.github.com>
2024-01-06 18:01:11 +0100

    Added first version of the backend - app.py. Added database connection. Added requirements.txt for pip install. Added .gitignore to ignore the virtual environment. Added index.html for testing purposes.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, jsonify
 from flask_mysqldb import MySQL
 import json
@@ -6,12 +7,22 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_PORT'] = 52000
+=======
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 53755 
+>>>>>>> a1f7ec8 (Added first version of the backend - app.py.)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'admin'
 app.config['MYSQL_DB'] = 'proj_grupowy'
 
 mysql = MySQL(app)
 
+<<<<<<< HEAD
 def get_from_db(query, isAnswer):
     tmp_response = []
     try:
@@ -43,3 +54,18 @@ def get_question(question_id):
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
 
+=======
+@app.route('/')
+def index():
+    try:
+        with mysql.connection.cursor() as cur:
+            cur.execute("SELECT * FROM Questions")
+            result = cur.fetchall()
+            answers = [{'question_id': row[0], 'question_text': row[1]} for row in result]
+        return render_template('index.html', data=answers)
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+>>>>>>> a1f7ec8 (Added first version of the backend - app.py.)
